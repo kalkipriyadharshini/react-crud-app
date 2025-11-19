a9bb4a5a6ad838ff6148f363b2622d6940edca6c
kalkipiyadharshini <237750792+Kalkipriyadharshinii@users.noreply.github.com>
2025-11-19 12:33:02 +0530

    creating crud page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import { useState,useEffect } from 'react';
+import {Button,EditableText} from '@blueprintjs/core';
+import "@blueprintjs/core/lib/css/blueprint.css";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  const[users,setUsers]=useState([])
+ useEffect(()=>{
+  fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response)=>response.json())
+  .then((json)=>setUsers(json))
+ },[])
+
+ return(
+  <div className='App'>
+    <table className='bp4-html-table modifier'>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>email</th>
+          <th>website</th>
+          <th>actions</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {users.map(user=>
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td><EditableText value={user.email} /></td>
+            <td><EditableText value={user.website} /></td>
+            <td>
+              <Button intent='primary'>Update</Button>
+              <Button intent='danger'>Delete</Button>
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  </div>
+ )
 }
 
-export default App;
+export default  App;
